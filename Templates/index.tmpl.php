98f5da87ee4ce04f2d047de206c646f23c1f4db5
shadowjack <kostyap@hotmail.ru>
2014-09-26 12:31:42 +0400

    make download links active only if file is ready

--- Templates/index.tmpl.php
+++ Templates/index.tmpl.php
@@ -7,13 +7,27 @@ echo "<table><thead>".
          "<td>Info</td>".
          "<td>Delete link</td>".
      "</thead><tbody>";
-foreach($videos as $row) {
-echo "<tr><td>".$row['id']."</td><td>".
-                $row['title']."</td><td>".
-                "<a href='".$row['id']."/flv'>FLV</a></td><td>".
-                "<a href='".$row['id']."/mp4'>MP4</a></td><td>".
-                "<a href='".$row['id']."/meta'>info</a></td><td>".
-                "<button onclick='deleteVideo(".$row['id'].")'>Delete</button></td></tr>";
+foreach($videos as $row) 
+{
+    echo "<tr><td>".$row['id']."</td><td>".$row['title']."</td><td>";
+    if ( $row['FLV'] !== null && $row['FLV'] !== '' )
+    {
+        echo "<a href='".$row['id']."/flv'>FLV</a></td><td>";
+    }
+    else
+    {
+        echo "FLV</td><td>";
+    }
+    if ( $row['MP4'] !== null && $row['MP4'] !== '' )
+    {
+        echo "<a href='".$row['id']."/mp4'>MP4</a></td><td>";
+    }
+    else
+    {
+        echo "MP4</td><td>";
+    }
+    echo "<a href='".$row['id']."/meta'>info</a></td><td>".
+         "<button onclick='deleteVideo(".$row['id'].")'>Delete</button></td></tr>";
 }
 echo "</tbody></table>";
 echo "<a href='/video_converter/videos/new'>Add new video</a>"
